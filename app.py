e4f57107d089a98e1c8e58ca2a30c470ada08519
Omri Halfon <omrihalfon48@gmail.com>
2025-05-31 01:27:02 +0300

    Add PORT env for Render

--- app.py
+++ app.py
@@ -146,7 +146,8 @@ def index():
         </body>
     </html>
     """
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
